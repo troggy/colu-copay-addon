1bcd6cd6ad7fb1bd78dd28364f919ee2813c0a7c
Kosta Korenkov <kosta@whoot.me>
2016-09-21 09:26:42 +0300

    chore: remove unneeded code

--- js/init.js
+++ js/init.js
@@ -1,21 +1,5 @@
 
 angular.module('copayAddon.colu')
-    .config(function ($stateProvider) {
-      $stateProvider
-          .state('assets', {
-            url: '/assets',
-            walletShouldBeComplete: true,
-            needProfile: true,
-            views: {
-              'main': {
-                templateUrl: 'colu-copay-addon/views/assets.html'
-              }
-            },
-            onEnter: function($rootScope){
-              $rootScope.$emit('Local/SetTab', 'assets', false);
-            }
-          });
-    })
     .run(function (addonManager, coloredCoins, $state) {
       addonManager.registerAddon({
         formatPendingTxp: function(txp) {
