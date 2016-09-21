84f014f2c3d460a203b42615ea0c47fbc5623a1b
Kosta Korenkov <kosta@whoot.me>
2016-09-21 09:21:37 +0300

    feat: confirm tx popup to show asset details

---
+++ js/overrides/txFormatService.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('copayAddon.colu').config(function ($provide) {
+
+  $provide.decorator('txFormatService', function ($delegate, $rootScope) {
+    var defaultProcessTx = $delegate.processTx;
+
+    $delegate.processTx = function (tx) {
+      defaultProcessTx(tx);
+
+      tx.isAsset = tx.customData && tx.customData.asset;
+      if (tx.isAsset) {
+        tx.amountStr = tx.customData.asset.balanceStr;
+        tx.addressTo = tx.outputs[0].address;
+        tx.hasMultiplesOutputs = false;
+        tx.alternativeAmountStr = '';
+      }
+
+      return tx;
+    };
+    return $delegate;
+  });
+});
