e81ac7143f1c4c08ca936d077369616d554c1221
Kosta Korenkov <kosta@whoot.me>
2016-09-25 14:43:42 +0300

    feat: do not enforce backup on user

--- js/overrides/profileService.js
+++ js/overrides/profileService.js
@@ -5,36 +5,11 @@ angular.module('copayAddon.colu').config(function ($provide) {
   $provide.decorator('profileService', function ($delegate, $rootScope) {
     var defaultSetWalletClient = $delegate.setWalletClient;
 
-    $delegate.setWalletClient = function (credentials) {
-      defaultSetWalletClient(credentials);
-      var client = $delegate.walletClients[credentials.walletId];
-
-      if (!client) return;
-
-      var defaultBroadcastTxProposal = client.broadcastTxProposal.bind(client);
-
-      client.broadcastTxProposal = function (txp, opts, cb) {
-        if (txp.customData && txp.customData.financeTxId) {
-          var disableSuccessListener, disableErrorListener;
-          disableSuccessListener = $rootScope.$on('ColoredCoins/Broadcast:success', function() {
-            disableSuccessListener();
-            disableErrorListener();
-            defaultBroadcastTxProposal(txp, opts, cb);
-          });
-          disableErrorListener = $rootScope.$on('ColoredCoins/Broadcast:error', function(e, err) {
-            disableSuccessListener();
-            disableErrorListener();
-            cb(err);
-          });
-
-          $rootScope.$emit('ColoredCoins/BroadcastTxp', txp);
-        } else {
-          defaultBroadcastTxProposal(txp, opts, cb);
-        }
-      };
-
-      return client;
+    // do not enforce backups
+    $delegate.needsBackup = function(client, cb) {
+      return cb(false);
     };
+
     return $delegate;
   });
 });
